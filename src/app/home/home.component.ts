e15527380530c252b4ab65a6287c29d0f6c179e5
leandro-viana <leandrovianadc@gmail.com>
2018-11-06 10:46:26 -0200

    Module 5
    End of Module 5, introduction of Animations.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,14 +10,15 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
     trigger('goals',[
       transition('* => *', [
         query(':enter', style({ opacity:0}), {optional:true}),
-        query(':enter', stagger('300ms'), [
+
+        query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
             style({opacity:0, transform: 'translateY(-75%)', offset:0}),
             style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
             style({opacity:1, transform: 'translateY(0)', offset:1}),
           ]))]) , {optional:true}),
 
-          query(':leave', stagger('300ms'), [
+          query(':leave', stagger('300ms', [
             animate('.6s ease-in', keyframes([
               style({opacity:1, transform: 'translateY(0)', offset:0}),
               style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
