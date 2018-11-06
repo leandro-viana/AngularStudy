05dd8829cb6146484c61d2f7a088dd2caccf3d39
leandro-viana <leandrovianadc@gmail.com>
2018-11-06 11:36:32 -0200

    Final Commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,51 +8,53 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
   styleUrls: ['./home.component.scss'],
   animations: [
 
-    trigger('goals',[
+    trigger('goals', [
       transition('* => *', [
-        query(':enter', style({ opacity:0}), {optional:true}),
+        query(':enter', style({ opacity: 0 }), {optional: true}),
 
         query(':enter', stagger('300ms', [
           animate('.6s ease-in', keyframes([
-            style({opacity:0, transform: 'translateY(-75%)', offset:0}),
-            style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
-            style({opacity:1, transform: 'translateY(0)', offset:1}),
-          ]))]) , {optional:true}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+          ]))]), {optional: true}),
 
-          query(':leave', stagger('300ms', [
-            animate('.6s ease-in', keyframes([
-              style({opacity:1, transform: 'translateY(0)', offset:0}),
-              style({opacity:.5, transform: 'translateY(35px)', offset:.3}),
-              style({opacity:0, transform: 'translateY(-75%)', offset:1}),
-            ]))]) , {optional:true})
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
+          ]))]), {optional: true})
       ])
     ])
+
   ]
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText: string = 'Add an item';
+  goalText: string = 'My first life goal';
+  goals = [];
 
-    itemCount: number;
-    btnText: string = 'Add an item.';
-    goalText: string = 'My first life goal';
-    goals = ['My first life goal I want climb a mountain I want to skiing'];
-
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
     this.itemCount = this.goals.length;
-
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
-
+    this._data.changeGoal(this.goals);
   }
 
-  removeItem(i){
+  removeItem(i) {
     this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
   }
 
 }
